d9933fe5da29dae0bc64b6c7f6967c63ce3ab4cf
Francisco Peña <francisco.app@hotmail.com>
2016-08-20 16:19:30 -0600

    Se agrega seccion de captura de articulos en venta

--- public/js/controllers/modalSale.js
+++ public/js/controllers/modalSale.js
@@ -1,18 +1,36 @@
 app.controller('ModalSaleController',
-    function($scope, $uibModalInstance, sale, nextId, Utils, $scope, $http) {
+    function($scope, $uibModalInstance, sale, nextId, Utils, $scope, $http, $q, Configuration) {
         var saleModal = this;
         saleModal.alerts = [];
 
+        var getConfiguration = function() {
+            var deferred = $q.defer();
+            Configuration.get({}, function(configuration) {
+                saleModal.configuration = configuration;
+                deferred.resolve(true);
+            });
+            return deferred.promise;
+        };
+
         var initializeData = function() {
-            saleModal.title = 'Registro de Artículos';
-            saleModal.key = Utils.pad(nextId.id, 4);
-            saleModal.sale = {
-                client_id: '',
-                client_name: '',
-                total: '',
-                date: ''
-            };
-        }
+            $q.all([getConfiguration()]).then(function() {
+                saleModal.title = 'Registro de Artículos';
+                saleModal.key = Utils.pad(nextId.id, 4);
+                saleModal.articles = [];
+                saleModal.sale = {
+                    client_id: '',
+                    client_name: '',
+                    total: 0,
+                    date: ''
+                };
+                saleModal.info = {
+                    hitch: '0.00',
+                    hitchBonus: '0.00',
+                    total: '0.00'
+                };
+            });
+
+        };
 
         var addAlert = function(text, type) {
             saleModal.alerts.push({
@@ -23,6 +41,24 @@ app.controller('ModalSaleController',
                 saleModal.alerts.shift();
                 $scope.$apply();
             }, 3000);
+        };
+
+        var calculateArticlePrice = function(article) {
+            return article.price * (1 + (saleModal.configuration.rate * saleModal.configuration.deadline / 100));
+        };
+
+        var calculateInfo = function() {
+            var totalAmount = 0;
+            _.each(saleModal.articles, function(article) {
+                totalAmount += article.amount;
+            });
+            var hitch = (saleModal.configuration.hitch / 100) * totalAmount;
+            var hitchBonus = hitch * (saleModal.configuration.rate * saleModal.configuration.deadline / 100)
+            var total = totalAmount - hitch - hitchBonus;
+
+            saleModal.info.hitch = hitch.toFixed(2);
+            saleModal.info.hitchBonus = hitchBonus.toFixed(2);
+            saleModal.info.total = total.toFixed(2);
         };
 
         saleModal.save = function() {
@@ -84,9 +120,39 @@ app.controller('ModalSaleController',
             saleModal.selectedArticle = '';
         };
 
-        saleModal.addArticle = function () {
-        	// body...
+        saleModal.addArticle = function() {
+            if (saleModal.selectedArticle) {
+                if (saleModal.selectedArticle.existence) {
+                    var newArticle = angular.copy(saleModal.selectedArticle);
+                    newArticle.quantity = 1;
+                    newArticle.calculatedPrice = calculateArticlePrice(newArticle);
+                    newArticle.amount = newArticle.price * newArticle.quantity;
+                    saleModal.articles.push(newArticle);
+                    calculateInfo();
+                } else {
+                    addAlert('El artículo seleccionado no cuenta con existencia, favor de verificar', 'danger');
+                }
+                saleModal.cleanArticle();
+                saleModal.selectedArticleText = '';
+            } else {
+                addAlert('No a seleccionado un artículo', 'danger');
+            }
         };
-        
+
+        saleModal.calculateArticleAmout = function(article) {
+            if (article.quantity) {
+                article.amount = article.quantity * article.calculatedPrice;
+            } else {
+                addAlert('La cantidad del artículo debe ser mayor a 0 y menor o igual a ' + article.existence, 'danger');
+                article.quantity = 1;
+            }
+            calculateInfo();
+        };
+
+        saleModal.removeArticle = function (index) {
+        	saleModal.articles.splice(index, 1);
+        	calculateInfo();
+        };
+
         initializeData();
     });
