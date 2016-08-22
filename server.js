bd3a2cc9c69fe895cee3895f3ef23328aa3d162a
Francisco Antonio Peña Pérez <Franciscoapp92@gmail.com>
2016-08-22 08:32:58 -0600

    prueba server puerto

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -26,6 +26,6 @@ app.use(express.static(__dirname + '/node_modules'));
 app.get('/',function(req,res){
 	res.send({'message':'hello world!'});
 });
-app.listen(3000, function() {
+app.listen(8080, function() {
     console.log('Listening port ' + app.get('port'));
 });
