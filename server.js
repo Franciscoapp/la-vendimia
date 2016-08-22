2aaf14d936f271812d6ce7e18b6df556933cd654
Francisco Antonio Peña Pérez <Franciscoapp92@gmail.com>
2016-08-22 13:54:46 -0600

    Se toma puerto por defecto

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.get('/*', function(req, res) {
     res.sendFile(__dirname + '/public/views/index.html');
 });
 
-app.listen(8080, function() {
+app.listen(process.env.PORT || 8080, function() {
     console.log('Listening port ' + app.get('port'));
 });
